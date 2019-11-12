57a5c6c8eb9876b93ce8402c83bd82fe2236480c
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-12 10:52:18 +0100

    outline 2 classes functionality in AppContnet

--- erasys-app/src/components/AppContent..jsx
+++ erasys-app/src/components/AppContent..jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+//Second Class for nested API extended user data
+//renders detailed user data
+//recieves user ID and constructs API call from that
+
+//First class for API fetched data
+//fetch data, JSON check, map each into DOM
+//renders basic user data
+//pass user ID into second user detail
+
+
 function AppContent() {
   return (
     <div className="app-content">
+
     </div>
   );
 }
