401122d82d0efa25c20519370f96dc9116f55001
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-12 10:41:50 +0100

    get rid of unnecassery boiler plate data. App two empty components AppHead & AppContent

---
+++ erasys-app/src/components/AppContent..jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AppContent() {
+  return (
+    <div className="app-content">
+    </div>
+  );
+}
+
+export default AppContent;
---
+++ erasys-app/src/components/AppHead.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppHead() {
+  return (
+    <div className="app-head">
+        <h1>Erasys</h1>
+    </div>
+  );
+}
+
+export default AppHead;
